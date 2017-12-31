7f52ffa9f1b9f864013f8ad53720f4e1f8ebbc38
Stefan Schinkel <mail@dreeg.org>
2017-12-31 11:36:09 +0100

    add sample config

---
+++ funkenlights/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+"""Flask factory."""
+from flask import Flask
+
+from . import views
+
+def create_app(config):
+    """Flask app factory."""
+
+    app = Flask(__name__)
+    app.register_blueprint(views.MAIN)
+    app.config['binaries'] = config['binaries']
+    app.config['devices'] = config['devices']
+    app.config['groups'] = config['groups']
+    app.run(host='0.0.0.0', debug=True)
+
+    return app
