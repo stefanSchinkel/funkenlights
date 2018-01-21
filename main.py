b256cc08e611769329773d0464a555356d818c08
Stefan Schinkel <mail@dreeg.org>
2018-01-21 20:30:40 +0100

    shorten entrypoint

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import json
 
 from funkenlights import create_app
 
-with open("config.json") as fp:
-    cfg = json.load(fp)
-
 if __name__ == '__main__':
-    app = create_app(cfg)
+with open("config.json") as fp:
+    app = create_app(json.load(fp))
